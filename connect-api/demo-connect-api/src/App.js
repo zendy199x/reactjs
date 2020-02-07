7684ff31eb4a298608ee9f911f8312193d25333e
Nguyễn Văn Đức <nguyenduc.051194@gmail.com>
2020-02-08 01:20:44 +0700

    ui building

--- connect-api/demo-connect-api/src/App.js
+++ connect-api/demo-connect-api/src/App.js
@@ -1,26 +1,72 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from "react"
+import "./App.css"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+	render() {
+		return (
+			<div>
+				<div className="navbar navbar-default">
+					<a className="navbar-brand">Call API</a>
+					<ul className="nav navbar-nav">
+						<li className="active">
+							<a>Trang chủ</a>
+						</li>
+						<li>
+							<a>Quản lý sản phẩm</a>
+						</li>
+					</ul>
+				</div>
+
+				<div className="container">
+					<div className="row">
+						<div className="col-xs-12 col-sm-12 col-md-12 col-lg-12">
+							<button type="button" className="btn btn-info mb-10">
+								Thêm sản phẩm
+							</button>
+							<div className="panel panel-primary">
+								<div className="panel-heading">
+									<h3 className="panel-title">Danh sách sản phẩm</h3>
+								</div>
+								<div className="panel-body">
+									<table className="table table-bordered table-hover">
+										<thead>
+											<tr>
+												<th>STT</th>
+												<th>Mã</th>
+												<th>Tên</th>
+												<th>Giá</th>
+												<th>Trạng thái</th>
+												<th>Hành động</th>
+											</tr>
+										</thead>
+										<tbody>
+											<tr>
+												<td>1</td>
+												<td>1</td>
+												<td>iPhone X</td>
+												<td>500</td>
+												<td>
+													<span className="label label-warning">Còn hàng</span>
+												</td>
+												<td>
+													<button type="button" className="btn btn-success mr-10">
+														Sửa
+													</button>
+                          <button type="button" className="btn btn-danger">
+														Xóa
+													</button>
+												</td>
+											</tr>
+										</tbody>
+									</table>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		)
+	}
 }
 
-export default App;
+export default App
