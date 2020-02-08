9a916c78473e4909b7e47c19be310587ece36e3b
Nguyễn Văn Đức <nguyenduc.051194@gmail.com>
2020-02-08 23:04:42 +0700

    using axios call api

--- connect-api/demo-connect-api/src/pages/ProductListPage/ProductListPage.js
+++ connect-api/demo-connect-api/src/pages/ProductListPage/ProductListPage.js
@@ -2,10 +2,24 @@ import React, {Component} from "react"
 import ProductList from "./../../components/ProductList/ProductList"
 import ProductItem from "./../../components/ProductItem/ProductItem"
 import {connect} from "react-redux"
+import axios from "axios"
 
 class ProductListPage extends Component {
 	render() {
-		const {products} = this.props
+		// const {products} = this.props
+		let products = []
+		axios({
+			method: "GET",
+			url: "http://localhost:3000/products",
+			data: null
+		})
+			.then(res => {
+				console.log(res)
+				products = res.data
+			})
+			.catch(err => {
+				console.log(err)
+			})
 		return (
 			<div className="col-xs-12 col-sm-12 col-md-12 col-lg-12">
 				<button type="button" className="btn btn-info mb-10">
