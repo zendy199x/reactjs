86e1fc2cf5c731db0fc54cf9d2520994b9e50e71
Nguyễn Văn Đức <nguyenduc.051194@gmail.com>
2020-02-08 23:35:26 +0700

    axios general

--- connect-api/demo-connect-api/src/pages/ProductListPage/ProductListPage.js
+++ connect-api/demo-connect-api/src/pages/ProductListPage/ProductListPage.js
@@ -2,24 +2,28 @@ import React, {Component} from "react"
 import ProductList from "./../../components/ProductList/ProductList"
 import ProductItem from "./../../components/ProductItem/ProductItem"
 import {connect} from "react-redux"
-import axios from "axios"
+import callApi from "./../../utils/apiCaller"
 
 class ProductListPage extends Component {
-	render() {
-		// const {products} = this.props
-		let products = []
-		axios({
-			method: "GET",
-			url: "http://localhost:3000/products",
-			data: null
+	constructor(props) {
+		super(props)
+		this.state = {
+			products: []
+		}
+	}
+
+	componentDidMount() {
+		callApi("products", "GET", null).then(res => {
+			this.setState({
+				products: res.data
+			})
 		})
-			.then(res => {
-				console.log(res)
-				products = res.data
-			})
-			.catch(err => {
-				console.log(err)
-			})
+	}
+
+	render() {
+		// var { products } = this.props;
+		const {products} = this.state
+
 		return (
 			<div className="col-xs-12 col-sm-12 col-md-12 col-lg-12">
 				<button type="button" className="btn btn-info mb-10">
@@ -31,10 +35,10 @@ class ProductListPage extends Component {
 	}
 
 	showProducts = products => {
-		let result = null
+		var result = null
 		if (products.length > 0) {
 			result = products.map((product, index) => {
-				return <ProductItem key={index} index={index} product={product} />
+				return <ProductItem key={index} product={product} index={index} />
 			})
 		}
 		return result
