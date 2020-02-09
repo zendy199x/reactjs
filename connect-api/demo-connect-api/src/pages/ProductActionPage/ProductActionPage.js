af0dd630eda77782f94b5c85739b02b10e9d1350
Nguyễn Văn Đức <nguyenduc.051194@gmail.com>
2020-02-09 10:55:09 +0700

    add product 1

--- connect-api/demo-connect-api/src/pages/ProductActionPage/ProductActionPage.js
+++ connect-api/demo-connect-api/src/pages/ProductActionPage/ProductActionPage.js
@@ -1,20 +1,63 @@
 import React, {Component} from "react"
-import ProductList from "./../../components/ProductList/ProductList"
-import ProductItem from "./../../components/ProductItem/ProductItem"
+import callApi from "./../../utils/apiCaller"
 
 class ProductActionPage extends Component {
+	constructor(props) {
+		super(props)
+		this.state = {
+			id: "",
+			txtName: "",
+			txtPrice: "",
+			chkbStatus: ""
+		}
+	}
+
+	onChange = e => {
+		const target = e.target
+		const name = target.name
+		const value = target.type === "checkbox" ? target.checked : target.value
+		this.setState({
+			[name]: value
+		})
+	}
+
+	onSave = e => {
+		e.preventDefault()
+		const {txtName, txtPrice, chkbStatus} = this.state
+		callApi("products", "POST", {
+			name: txtName,
+			price: txtPrice,
+			status: chkbStatus
+		}).then(res => {
+			console.log(res)
+		})
+	}
+
 	render() {
+		const {txtName, txtPrice, chkbStatus} = this.state
 		return (
 			<div className="col-xs-6 col-sm-6 col-md-6 col-lg-6">
-				<form>
+				<form onSubmit={this.onSave}>
 					<div className="form-group">
 						<label>Tên sản phẩm</label>
-						<input type="text" className="form-control" />
+						<input
+							type="text"
+							className="form-control"
+							name="txtName"
+							value={txtName}
+							onChange={this.onChange}
+						/>
 					</div>
 
 					<div className="form-group">
 						<label>Giá</label>
-						<input type="number" className="form-control" />
+						<input
+							type="number"
+							className="form-control"
+							name="txtPrice"
+							value={txtPrice}
+							onChange={this.onChange}
+						/>
 					</div>
 
 					<div className="form-group">
@@ -23,7 +66,12 @@ class ProductActionPage extends Component {
 
 					<div className="checkbox">
 						<label>
-							<input type="checkbox" />
+							<input
+								type="checkbox"
+								name="chkbStatus"
+								value={chkbStatus}
+								onChange={this.onChange}
+							/>
 							Còn hàng
 						</label>
 					</div>
--- connect-api/demo-connect-api/src/pages/ProductListPage/ProductListPage.js
+++ connect-api/demo-connect-api/src/pages/ProductListPage/ProductListPage.js
@@ -1,4 +1,5 @@
 import React, {Component} from "react"
+import {Link} from "react-router-dom"
 import ProductList from "./../../components/ProductList/ProductList"
 import ProductItem from "./../../components/ProductItem/ProductItem"
 import {connect} from "react-redux"
@@ -26,9 +27,9 @@ class ProductListPage extends Component {
 
 		return (
 			<div className="col-xs-12 col-sm-12 col-md-12 col-lg-12">
-				<button type="button" className="btn btn-info mb-10">
+				<Link to="/product/add" className="btn btn-info mb-10">
 					Thêm sản phẩm
-				</button>
+				</Link>
 				<ProductList>{this.showProducts(products)}</ProductList>
 			</div>
 		)
