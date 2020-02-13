943a1bc7fa98914a39f6bb5c7ddca4272a7e8cb2
Nguyễn Văn Đức <nguyenduc.051194@gmail.com>
2020-02-13 23:45:46 +0700

    add middleware - redux thunk

--- connect-api/demo-connect-api/src/index.js
+++ connect-api/demo-connect-api/src/index.js
@@ -1,25 +1,32 @@
-import React from "react"
-import ReactDOM from "react-dom"
-import "./index.css"
-import App from "./App"
-import * as serviceWorker from "./serviceWorker"
-import {createStore} from "redux"
-import appReducers from "./reducers/index"
-import {Provider} from "react-redux"
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import * as serviceWorker from './serviceWorker';
+import { createStore, compose, applyMiddleware } from 'redux';
+import appReducers from './reducers/index';
+import { Provider } from 'react-redux';
+import thunk from 'redux-thunk';
+
+
+// const store = createStore(
+//     appReducers,
+//     window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
+//     applyMiddleware(thunk)
+// );
+
+const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
 const store = createStore(
-	appReducers,
-	window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-)
+    appReducers, /* reducer */
+    composeEnhancer(
+        applyMiddleware(thunk), /* enhancer(middleware) */
+    )
+);
 
 ReactDOM.render(
-	<Provider store={store}>
-		<App />
-	</Provider>,
-	document.getElementById("root")
-)
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister()
+    <Provider store={store}>
+        <App />
+    </Provider>, document.getElementById('root'));
+    
+serviceWorker.unregister();
