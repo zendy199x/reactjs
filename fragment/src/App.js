5424ea3e00257c64344448cf4e1fb250025889b6
Nguyễn Văn Đức <nguyenduc.051194@gmail.com>
2020-03-01 21:47:05 +0700

    fragment

---
+++ fragment/src/App.js
@@ -0,0 +1,13 @@
+import React from "react"
+
+function App() {
+	return (
+		<React.Fragment>
+			<h1>Lập trình ReactJS</h1>
+			<h1>Lập trình VueJS</h1>
+			<h1>Lập trình Flutter</h1>
+		</React.Fragment>
+	)
+}
+
+export default App
