1ac706285d4d09a1d942d87e3d9220743e7e9632
Nguyễn Văn Đức <nguyenduc.051194@gmail.com>
2020-03-02 16:29:31 +0700

    build ui 1

--- redux-saga-marterial-ui-redux-form/src/container/Taskboard/index.js
+++ redux-saga-marterial-ui-redux-form/src/container/Taskboard/index.js
@@ -1,16 +1,38 @@
 import React, {Component} from "react"
 import {withStyles} from "@material-ui/core"
 import styles from "./styles"
+import Button from "@material-ui/core/Button"
+import AddIcon from "@material-ui/icons/Add"
+import Grid from "@material-ui/core/Grid"
+import {STATUSES} from "./../../constains"
 
 class Taskboard extends Component {
+	renderBoard() {
+		let xhtml = null
+		xhtml = (
+			<Grid container spacing={2}>
+				{STATUSES.map((status, index) => {
+					return (
+						<Grid item md={4} xs={12} key={index}>
+							{status.label}
+						</Grid>
+					)
+				})}
+			</Grid>
+		)
+		return xhtml
+	}
+
 	render() {
-        const {classes} = this.props
+		const {classes} = this.props
 		return (
-            <div className={classes.taskboard}>
-            <div className={classes.shape}>ReactJS</div>
-            <div className={classes.shape}>VueJS</div>
-            </div>
-            )
+			<div className={classes.taskboard}>
+				<Button variant="contained" color="primary" className={classes.button}>
+					<AddIcon /> Thêm mới công việc
+				</Button>
+				{this.renderBoard()}
+			</div>
+		)
 	}
 }
 
--- redux-saga-marterial-ui-redux-form/src/container/Taskboard/styles.js
+++ redux-saga-marterial-ui-redux-form/src/container/Taskboard/styles.js
@@ -1,18 +1,5 @@
 const styles = theme => ({
-	taskboard: {
-		display: "flex",
-		alignItems: "center"
-	},
-	shape: {
-		// backgroundColor: "red",
-		// color: "white",
-        // borderColor: "#cccccc",
-        padding: 20,
-        margin: 10,
-        // borderRadius: 4
-        backgroundColor: theme.color.primary,
-        color: theme.shape.textColor
-	}
+    
 })
 
 export default styles
