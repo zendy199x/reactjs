cd0697a5902563b006bc4d4b75c8b53570138101
Nguyễn Văn Đức <nguyenduc.051194@gmail.com>
2020-04-14 15:10:45 +0700

    setup rredux saga

--- redux-saga-marterial-ui-redux-form/src/redux/configureStore.js
+++ redux-saga-marterial-ui-redux-form/src/redux/configureStore.js
@@ -1,6 +1,8 @@
 import { createStore, compose, applyMiddleware } from "redux";
 import thunk from "redux-thunk";
 import rootReducer from "./../reducers";
+import createSagaMiddleware from "redux-saga";
+import rootSaga from "./../sagas";
 
 const composeEnhancers =
     process.env.NODE_ENV !== "production" &&
@@ -11,10 +13,13 @@ const composeEnhancers =
           })
         : compose;
 
+const sagaMiddleware = createSagaMiddleware();
+
 const configureStore = () => {
-    const middlewares = [thunk];
+    const middlewares = [thunk, sagaMiddleware];
     const enhancers = [applyMiddleware(...middlewares)];
     const store = createStore(rootReducer, composeEnhancers(...enhancers));
+    sagaMiddleware.run(rootSaga);
     return store;
 };
 
---
+++ redux-saga-marterial-ui-redux-form/src/sagas/index.js
@@ -0,0 +1,6 @@
+function* rootSaga() {
+    yield true;
+    console.log("This is root saga");
+}
+
+export default rootSaga;
