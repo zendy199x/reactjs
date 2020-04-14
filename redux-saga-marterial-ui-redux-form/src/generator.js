d87043ae7f59c921bd1f9fdeb520efbce484538b
Nguyễn Văn Đức <nguyenduc.051194@gmail.com>
2020-04-14 14:52:42 +0700

    generator function p2

--- redux-saga-marterial-ui-redux-form/src/generator.js
+++ redux-saga-marterial-ui-redux-form/src/generator.js
@@ -17,3 +17,36 @@ console.log("result 3", resullt.next()); //undefined - done: true
 // console.log("result 1", helloGeneratorFunction().next()); //2020 - done: false
 // console.log("result 2", helloGeneratorFunction().next()); //2020 - done: false
 // console.log("result 3", helloGeneratorFunction().next()); //2020 - done: false
+
+console.log("Vòng lặp vô tận");
+
+function* infiniteLoop() {
+    while (true) {
+        //callback
+        yield "Tôi đang lắng nghe...";
+    }
+}
+
+const iterator = infiniteLoop();
+
+console.log("Infinite 1", iterator.next());
+console.log("Infinite 2", iterator.next());
+console.log("Infinite 3", iterator.next());
+
+console.log("Generator function: Generator trong generator");
+//Xin chào => Generator -  Redux Saga =>  Kết thúc
+
+function* printName() {
+    yield "Generator - Redux Saga";
+}
+
+function* hello() {
+    yield "Xin chào";
+    yield* printName();
+    yield "Kết thúc";
+}
+
+const iteratorGenerator = hello();
+console.log("Iterator 1", iteratorGenerator.next());
+console.log("Iterator 2", iteratorGenerator.next());
+console.log("Iterator 3", iteratorGenerator.next());
