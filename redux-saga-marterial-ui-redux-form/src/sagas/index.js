4373158d467b89614c212c4ccf94098132b394ce
Nguyễn Văn Đức <nguyenduc.051194@gmail.com>
2020-04-14 15:32:37 +0700

    redux sagga fork

--- redux-saga-marterial-ui-redux-form/src/sagas/index.js
+++ redux-saga-marterial-ui-redux-form/src/sagas/index.js
@@ -1,6 +1,18 @@
-function* rootSaga() {
+import { fork } from "redux-saga/effects";
+
+function* watchFetchListTaskAction() {
     yield true;
-    console.log("This is root saga");
+    console.log("Watching fetch list task");
+}
+
+function* watchCreateTaskAction() {
+    yield true;
+    console.log("Watching create task action");
+}
+
+function* rootSaga() {
+    yield fork(watchFetchListTaskAction);
+    yield fork(watchCreateTaskAction);
 }
 
 export default rootSaga;
